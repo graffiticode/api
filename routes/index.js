8eed0a184635bc4d1bda1cdb3166d1971c117af8
Jeff Dyer <jeffdyer@acm.org>
2019-06-17 13:18:43 -0700

    Remove unused files

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
-module.exports.label = require('./label');
-module.exports.stat = require('./stat');
 module.exports.form = require('./form');
 module.exports.data = require('./data');
+module.exports.code = require('./code');
 module.exports.d = require('./d');
