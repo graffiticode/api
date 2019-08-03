a80db3861c8121a631692f1f2dca538eed23cdfb
Kevin Dyer <kevindy@google.com>
2019-08-03 10:12:13 -0600

    cleaned up lang functions (opinionated clean up)

--- src/lang.js
+++ src/lang.js
@@ -1,65 +1,63 @@
 const {getCompilerHost, getCompilerPort} = require('./util');
 
-let compilerVersions = {};
+const pingCache = new Map();
+const compilerVersions = new Map();
 
 function getCompilerVersion(lang, resume) {
   // Compiler version tells which parser to use.
-  if (compilerVersions[lang]) {
-    resume(compilerVersions[lang]);
-  } else {
-    pingLang(lang, (pong) => {
-      if (pong) {
-        var data = [];
-        var options = {
-          host: getCompilerHost(lang, global.config),
-          port: getCompilerPort(lang, global.config),
-          path: "/version",
-        };
-        try {
-          var req = global.protocol.get(options, function(res) {
-            res.on("data", function (chunk) {
-              data.push(chunk);
-            }).on("end", function () {
-              let str = data.join("");
-              let version = parseInt(str.substring(1));
-              version = compilerVersions[lang] = isNaN(version) ? 0 : version;
-              resume(version);
-            }).on("error", () => {
-              resume(null);
-            });
-          });
-        } catch (e) {
-          console.log("ERROR " + e.stack);
-          resume(null);
-        }
-      } else {
-        resume(null);
-      }
-    });
+  if (compilerVersions.has(lang)) {
+    return resume(compilerVersions.get(lang));
   }
-}
-
-let pingCache = {};
-function pingLang(lang, resume) {
-  if (pingCache[lang]) {
-    resume(true);
-  } else {
-    let options = {
-      method: 'HEAD',
+  pingLang(lang, (pong) => {
+    if (!pong) {
+      return resume(null);
+    }
+    function onError(err) {
+      console.log(`ERROR language ${lang} version: ${err.message}`);
+      resume(null);
+    }
+    const options = {
       host: getCompilerHost(lang, global.config),
       port: getCompilerPort(lang, global.config),
-      path: '/'
+      path: '/version',
     };
-    req = global.protocol.request(options, function(r) {
-      let pong = r.statusCode === 200;
-      pingCache[lang] = pong;
-      resume(pong);
-    }).on("error", (e) => {
-      pingCache[lang] = false;
-      console.log("ERROR language unavailable: " + lang);
-      resume(false);
-    }).end();
+    const req = global.protocol.get(options, (res) => {
+      const data = [];
+      res.on('data', (chunk) => data.push(chunk));
+      res.on('end', () => {
+        const str = data.join('');
+        const version = parseInt(str.substring(1));
+        version = compilerVersions[lang] = isNaN(version) ? 0 : version;
+        resume(version);
+      })
+      res.on('error', onError);
+    });
+    req.on('error', onError);
+    req.end();
+  });
+}
+
+function pingLang(lang, resume) {
+  if (pingCache.has(lang) && pingCache.get(lang)) {
+    return resume(true);
   }
+  const options = {
+    method: 'HEAD',
+    host: getCompilerHost(lang, global.config),
+    port: getCompilerPort(lang, global.config),
+    path: '/'
+  };
+  const req = global.protocol.request(options, (res) => {
+    const pong = res.statusCode === 200;
+    pingCache.set(lang, pong);
+    resume(pong);
+  });
+  req.on('error', (err) => {
+    pingCache.set(lang, false);
+    console.log(`ERROR language ${lang} unavailable: ${err.message}`);
+    resume(false);
+  });
+  req.end();
 }
 
 exports.pingLang = pingLang;
