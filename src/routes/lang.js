13a593ae9834dfb22f54b75739ec3d1f6b2b9c3b
Kevin Dyer <kevin.m.dyer@gmail.com>
2020-04-07 20:37:11 -0600

    propogate lang statuscode for lang assets

--- src/routes/lang.js
+++ src/routes/lang.js
@@ -11,25 +11,25 @@ module.exports = () => {
     if (isNaN(parseInt(id))) {
       res.sendStatus(400);
     } else {
-      let lang = "L" + id;
+      let lang = 'L' + id;
       pingLang(lang, pong => {
         if (pong) {
           if (path === undefined) {
             res.sendStatus(200);
           } else {
-            let data = [];
             let config = global.config;
             let options = {
               host: getCompilerHost(lang, config),
               port: getCompilerPort(lang, config),
-              path: "/" + path,
+              path: '/' + path,
             };
-            req = protocol.get(options, function(res2) {
-              res2.on("data", function (chunk) {
-                data.push(chunk);
-              }).on("end", function () {
-                res.send(data.join(""));
-              });
+            req = protocol.get(options, (langRes) => {
+              const data = [];
+              langRes
+                .on('data', (chunk) => data.push(chunk))
+                .on('end', () => {
+                  res.status(langRes.statusCode).send(data.join(''));
+                });
             });
           }
         } else {
