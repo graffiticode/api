224e42a71008d2eab12301c4b19e040dc5236d8f
Jeff Dyer <jeffdyer@acm.org>
2021-04-26 12:28:41 -0700

    Remove src/deploy dir. Moved test files into test dirs

--- src/deploy/index.js
+++
@@ -1,7 +0,0 @@
-import awsServerlessExpress from 'aws-serverless-express';
-import app from './../app';
-import makeLambdaHandler from './lambda';
-
-const lambdaHandler = makeLambdaHandler({ awsServerlessExpress, app });
-
-export { lambdaHandler };
--- src/deploy/lambda.js
+++
@@ -1,6 +0,0 @@
-export default function makeLambdaHandler({ awsServerlessExpress, app }) {
-  const server = awsServerlessExpress.createServer(app);
-  return function lambdaHandler(event, context) {
-    awsServerlessExpress.proxy(server, event, context);
-  };
-}
