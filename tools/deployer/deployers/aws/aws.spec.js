b1e77b94ec02484071b05f6cb93455afca87afd3
Jeff Dyer <jeffdyer@acm.org>
2021-04-26 12:19:48 -0700

    Remove more deploy code

--- tools/deployer/deployers/aws/aws.spec.js
+++
@@ -1,8 +0,0 @@
-import { buildGetIAM } from "./aws";
-
-describe('aws', () => {
-  it('getIAM', () => {
-    const IAM = {};
-    const getIAM = buildGetIAM({ IAM });
-  });
-});
--- tools/deployer/runners/config.js
+++
@@ -1,8 +0,0 @@
-export default function buildMakeConfig({ flags, fsPromise }) {
-  return async function makeConfig({ }) {
-    if (flags.config) {
-      return JSON.parse(await fsPromise.readFile(flags.config));
-    }
-    throw new Error('must specify a config filepath');
-  };
-}
